7452ce65d3ee28ed96b458f686b1b00321abd190
Samuel Ocran <sammiestt@gmail.com>
2020-01-23 18:45:11 +0100

    basic server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => res.status(200).send({
+   message: 'welcome to The Blog API'
+}));
+
+app.listen(port, () => {
+   console.log(`Server is running on PORT ${port}`);
+});
+
+
+export default app;
